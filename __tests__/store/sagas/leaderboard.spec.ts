59396e6f9b568fd5cb1fcb00ef59cd7bceba0919
Anindya <anindya.themaster@gmail.com>
2019-02-06 01:26:06 +0100

    fix: saga test

---
+++ __tests__/store/sagas/leaderboard.spec.ts
@@ -0,0 +1,31 @@
+import { call } from 'redux-saga/effects';
+import { cloneableGenerator } from '@redux-saga/testing-utils';
+
+import {
+  fetchSeasonResult as seasonResultActionCreator
+} from '../../../src/store/actions/leaderboard';
+
+import {
+  fetchLeaderboard,
+  fetchSeasonResults
+} from '../../../src/store/sagas/leaderboard';
+
+import {
+  getLeaderboard,
+  getSeasonResults
+} from '../../../src/helpers/fetchResource';
+
+const selectedSeason = '2013';
+const seasonResultAction = seasonResultActionCreator(selectedSeason);
+
+describe('get leaderboard and season results flow', () => {
+  const leadGen = cloneableGenerator(fetchLeaderboard)();
+  it('should call leaderboard resouce', () => {
+    expect(leadGen.next().value).toEqual(call(getLeaderboard));
+  }); 
+
+  const seasonGen = cloneableGenerator(fetchSeasonResults)(seasonResultAction);
+  it('should call season result resouce with 2013', () => {
+    expect(seasonGen.next().value).toEqual(call(getSeasonResults, selectedSeason));
+  });
+});
--- src/store/sagas/leaderboard.ts
+++ src/store/sagas/leaderboard.ts
@@ -1,5 +1,4 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
-import axios from 'axios';
 
 import {
   LEADERBOARD_REQUESTED,
@@ -9,7 +8,7 @@ import {
 } from '../actionTypes';
 import { getLeaderboard, getSeasonResults } from '../../helpers/fetchResource';
 
-function* fetchLeaderboard() {
+export function* fetchLeaderboard() {
   try {
     const leaderboard = yield call(getLeaderboard);
     yield put({
@@ -24,7 +23,7 @@ function* fetchLeaderboard() {
   }
 }
 
-function* fetchSeasonResults(action) {
+export function* fetchSeasonResults(action) {
   const season = action.payload;
   try {
     const seasonResult = yield call(getSeasonResults, season);
